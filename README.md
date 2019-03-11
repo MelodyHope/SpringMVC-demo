476640aa0a35e22f2ab3d8e3c1dd426d11b2ca0e
Meldoy Chen <touchchen14@gmail.com>
2019-03-11 20:54:59 +0800

    测试TortoiseGit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SpringMVC-demo
 利用maven构建的基础SpringMVC框架
+测试
--- src/main/java/com/tch/test/LambdaTest.java
+++ src/main/java/com/tch/test/LambdaTest.java
@@ -3,6 +3,12 @@
  */
 package com.tch.test;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author hsadmin
  *
@@ -23,6 +29,26 @@ public class LambdaTest {
 		System.out.println(g.convert("20"));
 		g = Long::parseLong;
 		System.out.println(g.convert("25"));
+		
+		Function<String, Boolean> function = StringUtils::isBlank;
+		System.out.println(function.apply("sdfa"));
+		
+		Function<String, Long> function1 = Long::parseLong;
+		Function<Long, String> function2 = String::valueOf;
+		Function<String, String> andThenFunc = function1.andThen(function2);
+		System.out.println(andThenFunc.apply("1234"));
+		
+		Function<Integer, Integer> times2 = e -> e * 2;
+		Function<Integer, Integer> squared = e -> e * e;
+		int n = times2.compose(squared).apply(4); 
+		System.out.println(n);
+		n = times2.andThen(squared).apply(4);  
+		System.out.println(n);
+		
+		Predicate<String> predicate1 = String::isEmpty;
+		Predicate<Object> predicate2 = obj -> obj!=null;
+		Predicate<String> andpredicate = predicate1.and(predicate2);
+		System.out.println(andpredicate.test("234"));
 	}
 
 }
